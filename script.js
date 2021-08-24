f3ad92a58b0a67bf03250ad2ee6b76374ee7b504
gerlinp <gerlin.pierrelouis@gmail.com>
2021-08-24 10:26:56 -0400

    update

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-
 const nav = document.querySelector('.nav')
+const body = document.querySelector('#body')
+const navLinks = document.querySelectorAll('.nav-item')
+
+
+
+
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
@@ -11,3 +16,13 @@ function fixNav() {
         nav.classList.remove('navbar-dark')
     }
 }
+
+setTimeout(() => {
+    const ad = document.querySelector('.eapps-link')
+    const hide = document.querySelector('.hide')
+    ad.style.display = 'none'  
+    hide.style.display = 'block'
+}, 600);
+
+
+
